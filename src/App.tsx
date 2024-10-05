abfe12ab119c0fd6e7dd95fe58af025e449c84e2
Xyves <xodrixx@protonmail.com>
2024-10-05 22:16:50 +0200

    Create new route using blog id path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import { Blogs } from "./pages/Blogs/Blogs";
+import { Blog } from "./pages/Blogs/Blog";
 import { Layout } from "./Layout/Layout";
 import { Error } from "./pages/Error";
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blog/:blogId" element={<Blog />} />
         <Route path="/error" element={<Error />} />
       </Route>
     </Routes>
