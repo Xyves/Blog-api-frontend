6a9fb666a6b7361915abca31c0fe5583515701e8
Xyves <xodrixx@protonmail.com>
2024-10-11 21:48:37 +0200

    Add login and register routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Blogs } from "./pages/Blogs/Blogs";
 import { Blog } from "./pages/Blogs/Blog";
 import { Layout } from "./Layout/Layout";
 import { Error } from "./pages/Error";
+import Login from "./components/Login";
+import Register from "./components/Register";
 export const App = () => {
   return (
     <Routes>
@@ -11,6 +13,8 @@ export const App = () => {
         <Route index element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blog/:blogId" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/error" element={<Error />} />
       </Route>
     </Routes>
