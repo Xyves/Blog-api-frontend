73f8a29ac567d446cb9694e09022377c0c4dcbd6
Xyves <xodrixx@protonmail.com>
2024-10-12 21:46:37 +0200

    Add new content to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,24 +2,21 @@ export const Home = () => {
   return (
     <>
       <section>
-        <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
+        <div className="hero flex min-h-[calc(100vh-64px)] justify-center bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
             <img
               src="/images/hero.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
+              className="max-w-lg rounded-lg shadow-2xl"
             />
-            <div>
-              <h1 className="text-5xl font-bold"></h1>
-              <button className="btn-primary btn w-64">
+            <div className="mx-10   flex flex-col">
+              <h1 className="text-5xl font-bold">
+                Looking for fresh ideas or a quick, inspiring read? Our blog is
+                filled with content to entertain and inform.
+              </h1>
+              <button className="btn-primary btn  mt-5 w-64">
                 <a href="/blogs">Start Reading</a>
               </button>
-              <p className="py-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-                minima laboriosam maxime sed dignissimos harum provident itaque
-                fugiat. A repellat aliquid inventore dolor tempora, omnis
-                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
-                esse necessitatibus doloremque repudiandae!
-              </p>
+              <p className="py-5"></p>
             </div>
           </div>
         </div>
