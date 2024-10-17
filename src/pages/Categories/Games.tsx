1cb9792815fef570a39ef66ef2aaf917ba8eb8a4
Xyves <xodrixx@protonmail.com>
2024-10-17 20:52:27 +0200

    Create 3 category  components

--- src/pages/Categories/Games.tsx
+++ src/pages/Categories/Games.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { fetchBlogs } from "@/api/Blogs";
+import { PostList } from "@/components/Blog/PostList";
+import React, { useEffect, useState } from "react";
 
 export default function Games() {
-  return (
-    <div>Games</div>
-  )
+  const [blogPosts, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const url = "blog-api-backend-production-6489.up.railway.app/api/games";
+      const posts = await fetchBlogs(url);
+      if (posts) {
+        setBlogs(posts);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return <PostList posts={blogPosts} />;
 }
--- src/pages/Categories/Tech.tsx
+++ src/pages/Categories/Tech.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { fetchBlogs } from "@/api/Blogs";
+import { PostList } from "@/components/Blog/PostList";
+import React, { useEffect, useState } from "react";
 
 export default function Tech() {
-  return <div>Tech</div>;
+  const [blogPosts, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const url = "blog-api-backend-production-6489.up.railway.app/api/games";
+      const posts = await fetchBlogs(url);
+      if (posts) {
+        setBlogs(posts);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return <PostList posts={blogPosts} />;
 }
