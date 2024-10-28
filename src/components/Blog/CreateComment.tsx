2bf415ea1ba4cd3c473c448bd3f951d07c1dd0ec
Xyves <xodrixx@protonmail.com>
2024-10-28 20:30:02 +0100

    Create new components

---
+++ src/components/Blog/CreateComment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CreateComment({ user }) {
+  return (
+    <div>
+      <form action="">
+        <textarea name="" id="" cols="30"></textarea>
+        <button className="uppercase">post comment</button>
+      </form>
+    </div>
+  );
+}
