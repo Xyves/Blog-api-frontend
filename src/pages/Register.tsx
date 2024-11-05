c449411c7094414e50b29733493d61fecda17871
Xyves <xodrixx@protonmail.com>
2024-11-05 20:45:02 +0100

    Feat: Create signup page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,64 @@
 import { HandleRegisterSubmit } from "@/api/Auth";
 export default function Register() {
   return (
-    <form onSubmit={HandleRegisterSubmit} method="post">
-      <label htmlFor="nickname">Username</label>
-      <input type="text" name="nickname" id="nickname" required />
-
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" required />
-
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" required />
-
-      <button type="submit">Submit</button>
-    </form>
+    <section className="flex h-80 flex-col items-center justify-center">
+      <form
+        onSubmit={HandleRegisterSubmit}
+        method="post"
+        className="bg-gray-600 p-16"
+      >
+        <div className="labelWrap">
+          <label
+            htmlFor="nickname"
+            className="mr-5 inline-block  w-24 p-3 text-end "
+          >
+            Username:
+          </label>
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            required
+            className="bg-slate-800"
+          />
+        </div>
+        <div className="labelWrap">
+          <label
+            htmlFor="password"
+            className="mr-5 inline-block w-24 p-3 text-end "
+          >
+            Password:
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            className="bg-slate-800"
+          />
+        </div>
+        <div className="labelWrap">
+          <label
+            htmlFor="email"
+            className="mr-5 inline-block w-24 p-3 text-end"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            className="bg-slate-800"
+          />
+        </div>
+        <button
+          type="submit"
+          className="rounded-lg bg-orange-500 px-4 py-1 text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </section>
   );
 }
