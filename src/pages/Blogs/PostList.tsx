185e55d1b615184b204530d7a8d77ba1281ffc3c
Xyves <xodrixx@protonmail.com>
2025-04-05 15:37:17 +0200

    Add css changes

--- src/pages/Blogs/PostList.tsx
+++ src/pages/Blogs/PostList.tsx
@@ -6,8 +6,8 @@ const MemoizedPost = React.memo(Post);
 export const PostList = ({ posts }: { posts: PostInterface[] }) => {
   return (
     <div
-      className="mx-auto mt-8 grid min-h-full w-3/5 grid-cols-6 gap-4 p-6 child:col-span-6 sm:grid-cols-6 md:grid-cols-12 
-  md:child:col-span-6 lg:grid-cols-12 lg:child:col-span-4"
+      className="mx-auto mt-8 grid min-h-full grid-cols-6 gap-4 p-6  child:col-span-6 sm:mx-2 sm:w-full sm:grid-cols-6 md:w-[80%] md:grid-cols-12 
+  md:child:col-span-6 lg:w-3/5 lg:grid-cols-12 lg:child:col-span-4"
     >
       {posts.map((post, index) => {
         const variant = index % 7 === 6 ? "variant2" : "variant1";
